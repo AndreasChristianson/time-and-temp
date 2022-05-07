412dc54ad1641efd34f6d3574a629c2ec97760aa
Andreas Christianson <achristianson@pessimistic-it.com>
2022-05-07 12:22:01 -0500

    don't hide errors?

--- src/update_eink.py
+++ src/update_eink.py
@@ -3,18 +3,15 @@ import epd4in2
 import time_and_temp
 
 def update_eink():
-  try:
-    image = time_and_temp.get_image()
-    epd = epd4in2.EPD()
-    logging.debug("init...")
-    epd.init()
-    logging.debug("get frame...")
-    frame = epd.get_frame_buffer(image)
-    logging.debug("update...")
-    epd.display_frame(frame)
-    logging.debug("sleep...")
-    epd.sleep()
-    logging.debug("done")
+  image = time_and_temp.get_image()
+  epd = epd4in2.EPD()
+  logging.debug("init...")
+  epd.init()
+  logging.debug("get frame...")
+  frame = epd.get_frame_buffer(image)
+  logging.debug("update...")
+  epd.display_frame(frame)
+  logging.debug("sleep...")
+  epd.sleep()
+  logging.debug("done")
     
-  except IOError as e:
-    logging.warning(e)
