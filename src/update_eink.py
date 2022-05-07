6bba6cd4ea9268cf0d7cc51d8726bc1a24a21632
Andreas Christianson <achristianson@pessimistic-it.com>
2022-05-07 12:32:44 -0500

    run it!

--- src/update_eink.py
+++ src/update_eink.py
@@ -2,16 +2,17 @@ import logging
 import epd4in2
 import time_and_temp
 
-def update_eink():
-  image = time_and_temp.get_image()
-  epd = epd4in2.EPD()
-  logging.debug("init...")
-  epd.init()
-  logging.debug("get frame...")
-  frame = epd.get_frame_buffer(image)
-  logging.debug("update...")
-  epd.display_frame(frame)
-  logging.debug("sleep...")
-  epd.sleep()
-  logging.debug("done")
+logger = logging.getLogger('update_eink')
+logger.setLevel(logging.DEBUG)
+image = time_and_temp.get_image()
+epd = epd4in2.EPD()
+logger.debug("init...")
+epd.init()
+logger.debug("get frame...")
+frame = epd.get_frame_buffer(image)
+logger.debug("update...")
+epd.display_frame(frame)
+logger.debug("sleep...")
+epd.sleep()
+logger.debug("done")
     
