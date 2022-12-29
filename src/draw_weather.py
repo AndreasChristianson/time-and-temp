7f89458b122ad6299c0317fed9df92cabd3b94ad
Andreas Christianson <achristianson@pessimistic-it.com>
2022-12-29 11:12:21 -0600

    rework current temp

--- src/draw_weather.py
+++ src/draw_weather.py
@@ -1,4 +1,4 @@
-import grequests
+from requests import Session
 import fonts
 import layout
 
@@ -18,33 +18,35 @@ def draw_weather(draw):
 
 
 def get_data():
-    rs = (grequests.get(u) for u in urls)
-    responses = grequests.map(rs)
-    return map(lambda response: response.json(), responses)
+    session = Session()
+    forecast = session.get(urls[0])
+    current = session.get(urls[1])
+    return forecast.json(), current.json()
 
 
 def print_forecast(forecast, draw):
     if hasattr(forecast, 'properties'):
         print_period(forecast["properties"]["periods"][0], draw, 0)
-        print_period(forecast["properties"]["periods"][1], draw, 18+12+layout.margin)
-        print_period(forecast["properties"]["periods"][2], draw, (18+12+layout.margin)*2)
+        print_period(forecast["properties"]["periods"]
+                     [1], draw, 18+12+layout.margin)
+        print_period(forecast["properties"]["periods"]
+                     [2], draw, (18+12+layout.margin)*2)
 
 
 def print_period(period, draw, top):
-  layout.draw_centered(draw,period["name"]+": "+str(
-        period["temperature"])+"°"+period["temperatureUnit"]
-        ,fonts.small,top,layout.top_left_border+layout.margin,400-layout.top_left_border-layout.margin*2)
-  layout.draw_centered(draw,period["shortForecast"]
-        ,fonts.tiny,top+18,layout.top_left_border+layout.margin,400-layout.top_left_border-layout.margin*2)
+    layout.draw_centered(draw, period["name"]+": "+str(
+        period["temperature"])+"°"+period["temperatureUnit"], fonts.small, top, layout.top_left_border+layout.margin, 400-layout.top_left_border-layout.margin*2)
+    layout.draw_centered(draw, period["shortForecast"], fonts.tiny, top+18,
+                         layout.top_left_border+layout.margin, 400-layout.top_left_border-layout.margin*2)
 
 
 def print_current(current, draw):
-    print(current)
-    if hasattr(current["properties"]["temperature"], "value"):
-        temp = current["properties"]["temperature"]["value"]
-        layout.draw_centered(draw,str(round(c_to_f(temp), 1))+"°F",fonts.large,36+12+layout.margin,layout.margin,layout.top_left_border-layout.margin)
+    temp = current["properties"]["temperature"]["value"]
+    layout.draw_centered(draw, str(round(c_to_f(temp), 1))+"°F", fonts.large,
+                             36+12+layout.margin, layout.margin, layout.top_left_border-layout.margin)
     textDescription = current["properties"]["textDescription"]
-    layout.draw_centered(draw,textDescription,fonts.tiny,36+12+layout.margin+36,layout.margin,layout.top_left_border-layout.margin)
+    layout.draw_centered(draw, textDescription, fonts.tiny, 36+12 +
+                         layout.margin+36, layout.margin, layout.top_left_border-layout.margin)
 
 
 def c_to_f(c):
